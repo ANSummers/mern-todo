2cb8acd5efef3f7be58365bf8cf1b3491214f4e5
Abbie <abbie@summers.gg>
2021-05-28 17:57:12 +0200

    started on navbar

--- src/App.js
+++ src/App.js
@@ -11,7 +11,9 @@ function App() {
   return (
     <Router>
     <div className="container">
-      <h2>MERN-Stack Todo App</h2>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        
+      </nav>
     </div>
     <Route path="/" exact component={TodosList} />
     <Route path="/edit/:id" component={EditTodo} />
