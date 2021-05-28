d184075a5e373262e2c23c7c54d64cdca2539759
Abbie <abbie@summers.gg>
2021-05-28 13:32:36 +0200

    Initial set up with routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import CreateTodo from "./components/create-todo.component";
+import EditTodo from "./components/edit-todo.component";
+import TodosList from "./components/todos-list.component";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+    <div className="container">
+      <h2>MERN-Stack Todo App</h2>
     </div>
+    <Route path="/" exact component={TodosList} />
+    <Route path="/edit/:id" component={EditTodo} />
+    <Route path="/create" component={CreateTodo} />
+    </Router>
   );
 }
 
